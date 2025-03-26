0d710174e869bc30c7d2c46f278ff1837d8cb83a
vlad-cool <vlad-matveev2004@mail.ru>
2025-03-26 12:03:03 +0300

    bigint library changed

--- tasks/task_e/task_e.cpp
+++ tasks/task_e/task_e.cpp
@@ -1,19 +1,20 @@
 #include <mpi.h>
-#include <stdio.h>
 
-#include "../../common/BigNumber/src/bignumber.h"
+#include <iostream>
+
+// #include "../../common/BigNumber/src/bignumber.h"
+#include "../../common/bigint.h"
 
 int main(int argc, char *argv[])
 {
-    BigNumber b("5");   // BigNumber b is created with value 5.
-    BigNumber c("-20"); // BigNumber c is created with value -20.
-    BigNumber d("0");   // BigNumber d is created with value 0.
-    BigNumber e = b;    // BigNumber e is created with value 5.
-    BigNumber f = 30;   // BigNumber f is created with value 30.
-    // BigNumber g = "2060";   //BigNumber g is created with value 2060.
-    BigNumber h(22); // BigNumber h is created with value 22.
+    bigint ten = bigint("10");
+    bigint power = bigint("10");
+    bigint precision = big_pow(ten, power);
 
-    std::cout << b << std::endl;
+    bigint N = bigint("123");
+
+    std::cout << precision << std::endl;
+    std::cout << precision / N << std::endl;
 
     // int commsize, my_rank;
     // MPI_Init(&argc, &argv);
