6309ba391095b4d6a01e1ae17d279f6685b7068b
Vlad <vlad-matveev2004@mail.ru>
2025-04-22 23:25:59 +0300

    Hybrid game of life fixed (removed diamond inheritance)

--- tasks/task_gol/gol_hybrid.h
+++ tasks/task_gol/gol_hybrid.h
@@ -3,13 +3,23 @@
 
 #include <mpi.h>
 
-#include "gol_mpi.h"
 #include "gol_shared.h"
 
-class GameOfLifeHybrid : virtual public GameOfLifeMpi, public GameOfLifeShared
+class GameOfLifeHybrid : public GameOfLifeShared
 {
+protected:
+    size_t total_height;
+    int commsize;
+    int my_rank;
+    mutable uint8_t *buf;
+    void send(size_t line, int destination) const;
+    void recv(size_t line, int source);
+    void sync();
+    static int get_worker_height(size_t height, int commsize, int rank);
+
 public:
-    GameOfLifeHybrid(size_t width, size_t height, int commsize, int my_rank, size_t executors_per_process);
+    GameOfLifeHybrid(size_t width, size_t height, int commsize, int my_rank, size_t threads_per_process);
+    ~GameOfLifeHybrid();
     virtual void set_cell(size_t x, size_t y, bool val) override;
     void step(uint32_t steps = 1) override;
     void print() const override;
