b82502231fb87c274ad3080714383c29d7e5ec84
vlad-cool <vlad-matveev2004@mail.ru>
2025-04-30 12:00:07 +0300

    mpi implementation prototype + Shishka's equation

--- tasks/lab_1/mpi.cpp
+++ tasks/lab_1/mpi.cpp
@@ -5,21 +5,22 @@
 #include <cstddef>
 #include <cassert>
 #include <numbers>
+#include <mpi.h>
 
 #include "problem.h"
 #include "common.h"
 
-uint64_t **generate_workers(uint64_t chunk_t_n, uint64_t chunk_x_n)
+uint64_t **generate_workers(uint64_t chunk_x_n, uint64_t chunk_t_n)
 {
-    uint64_t *workers_tmp = new uint64_t[chunk_x_n * chunk_t_n];
     uint64_t **workers = new uint64_t *[chunk_t_n];
-    for (int64_t i = 0; i < chunk_t_n; i++)
+    workers[0] = new uint64_t[chunk_x_n * chunk_t_n];
+    for (int64_t i = 1; i < chunk_t_n; i++)
     {
-        workers[i] = workers_tmp + i * chunk_x_n;
+        workers[i] = workers[0] + i * chunk_x_n;
     }
     for (int64_t i = 0; i < chunk_x_n * chunk_t_n; i++)
     {
-        workers_tmp[i] = 0;
+        workers[0][i] = 0;
     }
 
     uint64_t index = 0;
@@ -75,52 +76,36 @@ uint64_t **generate_workers(uint64_t chunk_t_n, uint64_t chunk_x_n)
 
 int main(int argc, char *argv[])
 {
+    int32_t size, rank;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
     uint64_t x_n = static_cast<uint64_t>(std::ceil(x_max / x_step));
     uint64_t t_n = static_cast<uint64_t>(std::ceil(t_max / t_step));
 
     uint64_t chunk_x_size = 100;
     uint64_t chunk_t_size = 100;
+
     uint64_t chunk_x_n = (x_n + chunk_x_size - 1) / chunk_x_size;
     uint64_t chunk_t_n = (t_n + chunk_t_size - 1) / chunk_t_size;
-    x_n = chunk_x_n * chunk_x_size;
-    t_n = chunk_t_n * chunk_t_size;
 
-    uint64_t a = 7;
-    uint64_t b = 5;
-    uint64_t **workers = generate_workers(a, b);
-    std::cout << "Workers:\n";
-    for (uint64_t t_i = 0; t_i < a; t_i++)
-    {
-        for (uint64_t x_i = 0; x_i < b; x_i++)
-        {
-            std::cout << std::setw(2) << workers[t_i][x_i] << " ";
-        }
-        std::cout << std::endl;
-    }
+    chunk_x_n = (chunk_x_n + size - 1) / size * size;
+    chunk_t_n = (chunk_t_n + size - 1) / size * size;
 
-    a = 5;
-    b = 7;
-    workers = generate_workers(a, b);
-    std::cout << "Workers:\n";
-    for (uint64_t t_i = 0; t_i < a; t_i++)
-    {
-        for (uint64_t x_i = 0; x_i < b; x_i++)
-        {
-            std::cout << std::setw(2) << workers[t_i][x_i] << " ";
-        }
-        std::cout << std::endl;
-    }
+    x_n = chunk_x_n * chunk_x_size + 1;
+    t_n = chunk_t_n * chunk_t_size + 1;
 
-    return 0;
+    uint64_t **workers = generate_workers(chunk_x_n, chunk_t_n);
 
     x_step = x_max / x_n;
     t_step = t_max / t_n;
 
-    double *buf = new double[x_n * t_n];
     double **field = new double *[t_n];
-    for (uint64_t i = 0; i < t_n; i++)
+    field[0] = new double[x_n * t_n];
+    for (uint64_t i = 1; i < t_n; i++)
     {
-        field[i] = buf + i * x_n;
+        field[i] = field[0] + i * x_n;
     }
 
     for (uint64_t t_i = 0; t_i < t_n; t_i++)
@@ -132,16 +117,75 @@ int main(int argc, char *argv[])
         field[0][x_i] = phi(x_step * x_i);
     }
 
-    rectangle_scheme(field, 1, t_n, 1, x_n);
+    // uint64_t *buf = new uint64_t[chunk_t_size];
 
-    for (uint64_t t_i = 0; t_i < t_n; t_i += 10)
+    MPI_Status mpi_status;
+
+    uint64_t chunk_number = 0;
+    for (uint64_t chunk_t_i = rank; chunk_t_i < chunk_t_n; chunk_t_i += size)
     {
-        for (uint64_t x_i = 0; x_i < x_n; x_i += 10)
+        for (uint64_t chunk_x_i = 0; chunk_x_i < chunk_x_n; chunk_x_i++)
         {
-            std::cout << std::setw(8) << field[t_i][x_i] << " ";
+            if (rank == 0)
+            {
+                if (chunk_number >= size && chunk_number + chunk_t_n + size < chunk_x_n * chunk_t_n / size)
+                {
+                    MPI_Recv(
+                        field[(chunk_t_i + 1) * chunk_t_size] + (chunk_x_i + chunk_x_n - size) % chunk_x_n,
+                        chunk_x_size,
+                        MPI_DOUBLE,
+                        size - 1, 0,
+                        MPI_COMM_WORLD,
+                        &mpi_status);
+                }
+            }
+            else
+            {
+                if (chunk_t_i + size < chunk_t_n)
+                {
+                    MPI_Recv(
+                        field[chunk_t_i * chunk_t_size] + chunk_x_i * chunk_x_size,
+                        chunk_x_size,
+                        MPI_DOUBLE,
+                        rank - 1,
+                        0,
+                        MPI_COMM_WORLD,
+                        &mpi_status);
+                }
+            }
+
+            rectangle_scheme(field, chunk_t_i * chunk_t_size + 1, chunk_t_size, chunk_x_i * chunk_x_size + 1, chunk_x_size);
+
+            if (chunk_t_i + size < chunk_t_n)
+            {
+                MPI_Send(
+                    field[(chunk_t_i + 1) * chunk_t_size - 1] + chunk_x_n * chunk_x_size,
+                    chunk_x_size,
+                    MPI_DOUBLE,
+                    (rank + 1) % size,
+                    0,
+                    MPI_COMM_WORLD
+                );
+            }
+
+            chunk_number++;
         }
-        std::cout << std::endl;
-    };
+    }
+
+    if (rank == 0)
+    {
+        for (uint64_t t_i = 0; t_i < t_n; t_i += 10)
+        {
+            for (uint64_t x_i = 0; x_i < x_n; x_i += 10)
+            {
+                std::cout << std::setw(8) << field[t_i][x_i] << " ";
+            }
+            std::cout << std::endl;
+        }
+    }
+    else
+    {
+    }
 
 #ifdef DEBUG
     for (uint64_t i = 0; i < x_n * t_n)
@@ -149,8 +193,10 @@ int main(int argc, char *argv[])
     }
 #endif
 
+    delete[] field[0];
     delete[] field;
     delete[] workers[0];
     delete[] workers;
-    delete[] buf;
+
+    MPI_Finalize();
 }
--- tasks/lab_1/problem.cpp
+++ tasks/lab_1/problem.cpp
@@ -3,39 +3,62 @@
 
 #include "problem.h"
 
-double x_step = 1e-3;
-double t_step = 1e-3;
+double x_step = 1e-2;
+double t_step = 1e-2;
 
-double x_max = 10;
-double t_max = 10;
+double x_max = 100;
+double t_max = 100;
 
-double f(double x, double t)
-{
-    // if (t > 0.25)
-    // {
-    //     return (t - 0.25) / 10;
-    // }
-    // else
-    // {
-    //     return 0;
-    // }
-    // return x;
-    // return 0;
-    return 0.3;
-    // return std::sin(x * t);
-}
 
+const double c = 1.35; // should be positive
+\
 double phi(double x)
 {
-    // return 0;
-    // return 1;
-    return std::sin(x * std::numbers::pi * 4);
+    return std::sin(x/5);
 }
 
 double psi(double t)
 {
-    return t / 3;
-    // return 0;
-    // return 1 - std::cos(t / 5);
-    // return t;
+    return 2*std::sin(t);
 }
+
+double f(double x, double t)
+{
+    int s = static_cast<int>(x) % 100;
+    if((s < 40) || (s > 60))
+        return 0;
+    return 5*std::sin(x);
+}
+
+// double f(double x, double t)
+// {
+//     // if (t > 0.25)
+//     // {
+//     //     return (t - 0.25) / 10;
+//     // }
+//     // else
+//     // {
+//     //     return 0;
+//     // }
+//     // return x;
+//     // return 0;
+//     return 0.3;
+//     // return std::sin(x * t);
+// }
+
+// double phi(double x)
+// {
+//     return 0;
+//     // return 1;
+//     // return std::sin(x * std::numbers::pi * 4);
+// }
+
+// double psi(double t)
+// {
+//     // return 1;
+//     // return 1 - t / 10;
+//     // return t / 3;
+//     // return 0;
+//     return 1 - std::cos(t * 5 * std::numbers::pi);
+//     // return t;
+// }
