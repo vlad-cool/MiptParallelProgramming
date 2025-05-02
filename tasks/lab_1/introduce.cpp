bc7327d0b7ffb9684650108e066685cce9d09176
vlad-cool <vlad-matveev2004@mail.ru>
2025-05-02 15:25:47 +0300

    untroduction task

---
+++ tasks/lab_1/introduce.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <cassert>
+#include <chrono>
+#include <iomanip>
+#include <mpi.h>
+
+int main(int argc, char *argv[])
+{
+    int n = 64;
+    int buf_size = 4096 * 1024;
+    int size, rank;
+    
+    int *buf = new int[buf_size];
+    
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    
+    assert(size == 2 && "Expected running on exactly two processes");
+    
+    auto start = std::chrono::high_resolution_clock::now();
+    for (int i = 0; i < n; i++)
+    {
+        if (rank == 0)
+        {
+            MPI_Send(buf, buf_size, MPI_INT, 1, 0, MPI_COMM_WORLD);
+            MPI_Recv(buf, buf_size, MPI_INT, 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        }
+        if (rank == 1)
+        {
+            MPI_Recv(buf, buf_size, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            MPI_Send(buf, buf_size, MPI_INT, 0, 0, MPI_COMM_WORLD);
+        }
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+    duration /= 2;
+    duration /= n;
+    if (rank == 0)
+    {
+        std::cout << duration / 1000000 << "." << std::setfill('0') << std::setw(6) << duration % 1000000 << " seconds" << std::endl;
+    }
+
+    MPI_Finalize();
+
+    delete[] buf;
+}
