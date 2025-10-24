b425c7a35d94688d6fdd4f9852d88b3c29e84210
vlad-cool <vlad-matveev2004@mail.ru>
2025-10-24 16:29:27 +0300

    average works, no speed uo

--- tasks_omp_devices/src/task_avg.cpp
+++ tasks_omp_devices/src/task_avg.cpp
@@ -1,110 +1,74 @@
 #include <iostream>
+#include <iomanip>
 #include <random>
+#include <chrono>
 #include <omp.h>
 
-// int main()
-// {
-//     std::cout << omp_get_num_devices() << std::endl;
-
-//     size_t size = 1024 * 1024 * 256;
-
-//     int *a = new int[size];
-//     int *b = new int[size];
-
-//     std::random_device rd;
-//     std::mt19937 gen(rd());
-//     std::normal_distribution<int> dist(0.0, 1.0);
-
-//     for (size_t i = 0; i < size; i++)
-//     {
-//         a[i] = dist(gen);
-//     }
-// }
-
-int disp(int a[], size_t size, size_t teams)
+float avg_dev(float *a, size_t size)
 {
-    int avg_1 = 0, avg_2 = 0;
-#pragma omp target map(to : a[ : size])
-#pragma omp teams num_teams(teams)
+    float sum = 0.0;
+#pragma omp target map(to : a[ : size]) map(tofrom : sum)
+#pragma omp teams
     {
-        size_t chunk_size = (size + teams - 1) / teams;
-        size_t chunk_start = chunk_size * omp_get_team_num();
-        if (omp_get_team_num() == teams - 1)
+        if (omp_is_initial_device())
         {
-            chunk_size = size - chunk_start;
+            printf("Running on host\n");
         }
-        size_t chunk_end = chunk_start + chunk_size;
-
-        // printf("Running on device with %d teams in total and %d threads in each team\n", omp_get_num_teams(), omp_get_num_threads());
-
-        // std::cout << size << " " << chunk_start << " " << chunk_end << std::endl;
-        // printf("%zu %zu %zu\n", size, chunk_start, chunk_end);
-
-#pragma omp parallel for reduction(+ : avg_2)
-        // #pragma omp parallel for reduction(+ : avg_1, avg_2)
-        for (size_t i = chunk_start; i < chunk_end; i++)
-        {
-            // avg_1 += a[i];
-            avg_2 += a[i] * a[i];
-        }
-        // printf("   %lf %lf\n", avg_1, avg_2);
+#pragma omp parallel for reduction(+ : sum)
+        for (int i = 0; i < size; i++)
+            sum += a[i];
     }
+    return sum / size;
+}
 
-    return (avg_2 / size) - (avg_1 / size) * (avg_1 / size);
+float avg(float *a, size_t size)
+{
+    float sum = 0.0;
+    {
+        if (omp_is_initial_device())
+        {
+            printf("Running on host\n");
+        }
+
+#pragma omp parallel for reduction(+ : sum)
+        for (int i = 0; i < size; i++)
+            sum += a[i];
+    }
+    return sum / size;
 }
 
 int main()
 {
     std::cout << "Num devices: " << omp_get_num_devices() << std::endl;
-    size_t size = 1024 * 1024 * 256;
-    int *a = new int[size];
+    std::cout << omp_get_uid_from_device(0) << std::endl;
+
+    size_t size = 1024 * 1024 * 128;
+    // size_t size = 1024;
+
+    float *a = new float[size];
 
     std::random_device rd;
     std::mt19937 gen(rd());
-    // std::normal_distribution<int> dist(0.0, 1.0);
-    std::uniform_int_distribution<int> dist(0, 200);
+    std::normal_distribution<float> dist(10, 20);
 
     for (size_t i = 0; i < size; i++)
     {
         a[i] = dist(gen);
     }
 
-#pragma omp target
-    {
-        if (omp_is_initial_device())
-        {
-            printf("Running on host\n");
-        }
-        else
-        {
-            // int nteams = omp_get_num_teams();
-            // int nthreads = omp_get_num_threads();
-            // printf("Running on device with %d teams in total and %d threads in each team\n", nteams, nthreads);
+    float res;
 
-            // int *b = new int[size];
+    auto start = std::chrono::high_resolution_clock::now();
+    res = avg_dev(a, size);
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+    std::cerr << "Computer average of " << size << " floats on gpu in " << duration.count() / 1000000 << "." << std::setfill('0') << std::setw(6) << duration.count() % 1000000 << " seconds, result is " << res << std::endl;
 
-            std::cout << disp(a, size, 10) << std::endl;
-        }
-    }
+    start = std::chrono::high_resolution_clock::now();
+    res = avg(a, size);
+    end = std::chrono::high_resolution_clock::now();
+    duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+    std::cerr << "Computer average of " << size << " floats on cpu in " << duration.count() / 1000000 << "." << std::setfill('0') << std::setw(6) << duration.count() % 1000000 << " seconds, result is " << res << std::endl;
+
+    // std::cout < < < < std::endl;
 }
-
-// int main()
-// {
-//     std::cout << omp_get_num_devices() << std::endl;
-
-//     size_t size = 1024 * 1024 * 256;
-
-//     int *a = new int[size];
-//     int *b = new int[size];
-
-//     std::random_device rd;
-//     std::mt19937 gen(rd());
-//     std::normal_distribution<int> dist(0.0, 1.0);
-
-//     for (size_t i = 0; i < size; i++)
-//     {
-//         a[i] = dist(gen);
-//     }
-
-//     std::cout << disp(a, size, 8) << std::endl;
-// }
